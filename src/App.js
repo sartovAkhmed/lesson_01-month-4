16507fef330d33f3bf545a3e535939cf80454d2b
Akhmed <akhmedturan@gmail.com>
2023-11-09 20:02:25 +0600

    dz-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function MyApp() {
+  const [count, setCount] = useState(0)
+
+  function handeClick() {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
+      <MyButton count={count} onClick={handeClick}/>
+      <MyButton count={count} onClick={handeClick}/>
     </div>
-  );
+  )
 }
 
-export default App;
+function MyButton({count, onClick}) {
+  return <button onClick={onClick}>Clicked {count} times!</button>
+}
